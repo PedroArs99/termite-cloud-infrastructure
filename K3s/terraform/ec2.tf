2c48acd440a1955ce413e91fc5dcb8d861ee23e7
Pedro Arenas <pedroars99@icloud.com>
2023-01-14 00:48:02 +0100

    Open 31883 port

--- K3s/terraform/ec2.tf
+++ K3s/terraform/ec2.tf
@@ -148,11 +148,43 @@ resource "aws_security_group" "allow_https" {
   }
 }
 
+resource "aws_security_group" "allow_mqtt" {
+  name        = "allow_mqtt"
+  description = "Allow MQTT inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "MQTT from the internet"
+    from_port   = 31883
+    to_port     = 31883
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_mqtt"
+  }
+}
+
 resource "aws_network_interface" "nic" {
   subnet_id   = aws_subnet.subnet.id
   private_ips = ["172.16.10.100"]
 
-  security_groups = [aws_security_group.allow_ssh.id, aws_security_group.allow_k3s.id, aws_security_group.allow_http.id, aws_security_group.allow_https.id]
+  security_groups = [
+    aws_security_group.allow_ssh.id,
+    aws_security_group.allow_k3s.id,
+    aws_security_group.allow_http.id,
+    aws_security_group.allow_https.id,
+    aws_security_group.allow_mqtt.id
+  ]
 
   tags = {
     Name = var.name
@@ -165,10 +197,9 @@ resource "aws_key_pair" "root" {
 }
 
 resource "aws_instance" "ec2" {
-  ami                  = "ami-087924c9e0410af37" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
-  iam_instance_profile = aws_iam_instance_profile.cloud_watch_agent_profile.name
-  instance_type        = "t4g.small"
-  key_name             = aws_key_pair.root.key_name
+  ami           = "ami-087924c9e0410af37" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
+  instance_type = "t4g.small"
+  key_name      = aws_key_pair.root.key_name
 
   network_interface {
     network_interface_id = aws_network_interface.nic.id
@@ -195,9 +226,4 @@ resource "aws_instance" "ec2" {
 resource "aws_eip" "ec2_eip" {
   instance = aws_instance.ec2.id
   vpc      = true
-}
-
-resource "aws_iam_instance_profile" "cloud_watch_agent_profile" {
-  name = "CloudWatchAgentServer"
-  role = "CloudWatchAgentServerRole"
 }
